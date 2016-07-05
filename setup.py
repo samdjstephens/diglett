70a7c9b54d2b99ac080104b045e9f9363fb5d416
Sam Stephens <samdjstephens@gmail.com>
2016-07-05 09:33:39 +0100

    Python packaging initial

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='diglett',
+    version='0.1',
+    url='https://github.com/samdjstephens/diglett',
+    packages=find_packages(),
+    install_requires=[
+        'psutil'
+    ],
+    scripts=['diglett/diglett']
+)
